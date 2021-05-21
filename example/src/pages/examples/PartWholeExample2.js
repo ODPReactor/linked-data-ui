da5b496a151029801332468379a75e93c63946e5
colin <colin@ubuntu.com>
2021-05-21 10:57:42 -0400

    hotfix exporting components on index.js

--- example/src/pages/examples/PartWholeExample2.js
+++ example/src/pages/examples/PartWholeExample2.js
@@ -5,16 +5,16 @@ import { PartWhole } from "odp-reactor-visualframes"
 export function PartWholeExample2() {
     const whole = {
         uri: "https://arco.istc.cnr.it/ns/HistoricOrArtisticProperty/0100373516-0",
-        source: "http://arco.istc.cnr.it/visualPatterns/sparql"
+        source: "https://arco.istc.cnr.it/visualPatterns/sparql"
     }
     const parts = [
         {
             uri: "https://arco.istc.cnr.it/ns/HistoricOrArtisticProperty/0100373516-1",
-            source: "http://arco.istc.cnr.it/visualPatterns/sparql"
+            source: "https://arco.istc.cnr.it/visualPatterns/sparql"
         },
         {
             uri: "https://arco.istc.cnr.it/ns/HistoricOrArtisticProperty/0100373516-2",
-            source: "http://arco.istc.cnr.it/visualPatterns/sparql"
+            source: "https://arco.istc.cnr.it/visualPatterns/sparql"
         }
     ]
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Collection from "./visualframes/collection/Collection"
 import PartWhole from "./visualframes/partwhole/PartWhole"
+import Resource from "./resources/Resource"
+import Depiction from "./resources/Depiction"
+import Label from "./resources/Label"
 
-export { Collection, PartWhole }
+export { Collection, PartWhole, Resource, Depiction, Label }
